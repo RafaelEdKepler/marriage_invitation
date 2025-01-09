11c1a0be0ccfb6a83868146a328c5dffd4f22e83
Rafael Kepler <rafael.kepler@hotmail.com>
2025-01-09 15:22:56 -0300

    fix: reducing dgeg from trembling animation

--- src/pages/home/styles/style.ts
+++ src/pages/home/styles/style.ts
@@ -14,6 +14,8 @@ export const HalfSizedImg = styled.img<ImgDesignType>`
   transform-origin: ${props => props.direction};
   animation: ${props => props.vanished ? "openInvitation 2.5s forwards" : "invitationTrembling 4.5s"};
   animation-iteration-count: ${props => props.vanished ? "1" : "infinite"};
+  transition: transform 0.1s ease-in-out;
+  cursor: pointer;
 
   @keyframes openInvitation {
     0% {
@@ -26,17 +28,17 @@ export const HalfSizedImg = styled.img<ImgDesignType>`
   }
 
   @keyframes invitationTrembling {
-    0% { transform: translate(1px, 1px) rotate(0deg); opacity: 1}
-    10% { transform: translate(-1px, -2px) rotate(-1deg); }
-    20% { transform: translate(-2px, 0px) rotate(1deg); }
-    30% { transform: translate(2px, 1px) rotate(0deg); opacity: 0.8}
-    40% { transform: translate(1px, -1px) rotate(1deg); }
-    50% { transform: translate(-1px, 2px) rotate(-1deg); }
-    60% { transform: translate(-2px, 1px) rotate(0deg); opacity: 1}
-    70% { transform: translate(2px, 1px) rotate(-1deg); }
-    80% { transform: translate(-1px, -1px) rotate(1deg); }
-    90% { transform: translate(1px, 2px) rotate(0deg); opacity: 0.8}
-    100% { transform: translate(1px, -2px) rotate(-1deg); }
+    0% { transform: translate(0.5px, 0.5px) rotate(0deg); opacity: 1}
+    10% { transform: translate(0px, 0px) rotate(-0.3deg); }
+    20% { transform: translate(-0.5px, -0.5px) rotate(0.3deg); }
+    30% { transform: translate(0px, 0px) rotate(0deg); opacity: 0.8}
+    40% { transform: translate(0.5px, 0.5px) rotate(0.3deg); }
+    50% { transform: translate(0px, 0px) rotate(-0.3deg); }
+    60% { transform: translate(-0.5px, -0.5px) rotate(0deg); opacity: 1}
+    70% { transform: translate(0px, 0px) rotate(-0.3deg); }
+    80% { transform: translate(0.5px, 0.5px) rotate(0.3deg); }
+    90% { transform: translate(0px, 0px) rotate(0deg); opacity: 0.8}
+    100% { transform: translate(-0.5px, -0.5px) rotate(-0.3deg); }
   }
 
   @media (max-width: 430px) {
