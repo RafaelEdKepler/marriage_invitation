33c4d468980d1d748839d0d5ec1da77fe2a3e877
Rafael Kepler <rafael.kepler@hotmail.com>
2025-01-12 13:07:59 -0300

    including vercel.json file

--- src/router.tsx
+++ src/router.tsx
@@ -8,7 +8,7 @@ export default function Router() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/invitated" element={<Invitated />} />
-      <Route path="/special" element={<SpecialInvite />} />
+      <Route path="/special-invite/:groom" element={<SpecialInvite />} />
     </Routes>
   )
 }
