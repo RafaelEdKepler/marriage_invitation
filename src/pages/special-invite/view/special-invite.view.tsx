2ca74f5e839325b5a8b9f3b46afd6f21a71c9e40
Rafael Kepler <rafael.kepler@hotmail.com>
2025-01-12 12:56:17 -0300

    resolving deploy errors

--- src/pages/special-invite/view/special-invite.view.tsx
+++ src/pages/special-invite/view/special-invite.view.tsx
@@ -1,4 +1,3 @@
-import { useParams } from "react-router-dom";
 import { Container, HalfSizedImg, Section, VideoContainer } from "../styles/style";
 import { SpecialInviteType } from "../type";
 
@@ -6,8 +5,7 @@ export default function SpecialInviteView({ invitationShowed, invitationStatus, 
 
   const { innerWidth } = window;
 
-  const { groom } = useParams();
-
+  // const { groom } = useParams();
 
   return (
     <Section onClick={onInvitationClick}>
@@ -15,6 +13,7 @@ export default function SpecialInviteView({ invitationShowed, invitationStatus, 
       <HalfSizedImg vanished={invitationStatus} src="/direita.png" direction="right" />
       <Container invitationClicked={invitationShowed}>
         <VideoContainer>
+          {/* @ts-expect-error err */}
           <iframe width={innerWidth > 430 ? "800" : "250"} height={innerWidth > 430 ? "400" : "200"} src="https://www.youtube.com/embed/1sXx_GGeIBQ?si=lvLCYhdqaDsYu15F" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </VideoContainer>
       </Container>
