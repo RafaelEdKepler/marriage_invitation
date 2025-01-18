45157993f0a49b9385118b0ddd2c698a5a15f3d5
Rafael Eduardo Kepler <rafael.kepler@hotmail.com>
2025-01-18 16:56:38 -0300

    Atualizar o style.ts

--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -35,7 +35,7 @@ export const MenuOption = styled.div`
     text-decoration: none;
   }
 
-  @media (min-width: 630px) and (min-height: 650px) {
+  @media (max-width: 500px) and (max-height: 650px) {
     display: none !important;
   }
 `;
