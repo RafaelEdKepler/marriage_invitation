24f74d9d99ac431371787e9c1768c15663076987
Rafael Kepler <rafael.kepler@hotmail.com>
2025-01-20 17:49:45 -0300

    fix gif on mobile

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,9 +11,8 @@ export const GlobalStyles = createGlobalStyle`
   body {
     font-family: "EB Garamond", sans-serif;
     background-image: url('/casamento.gif'), url('/fundo_1.jpg');
-    background-repeat: repeat;
-    background-size: 100%;
-    height: 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
     display: flex;
     align-items: center;
     justify-content: center;
