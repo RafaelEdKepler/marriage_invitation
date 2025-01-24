52812b8adb88e2ac39036c9d3b9e4111a8eb216d
Rafael Kepler <rafael.kepler@hotmail.com>
2025-01-23 21:18:20 -0300

    feat videos

--- src/pages/special-invite/controller/special-invite.controller.tsx
+++ src/pages/special-invite/controller/special-invite.controller.tsx
@@ -15,7 +15,7 @@ export default function SpecialInviteController() {
   const showedVideo = {
     "lucas": "https://www.youtube.com/embed/akTXVYAgoAk?si=Hkj9Zfq64tqVhJAu",
     "heloisa": "https://www.youtube.com/embed/a9tX2svM7l4?si=MoiDNP9QXqpZeqGK",
-    "gabi": "",
+    "gabi": "https://www.youtube.com/embed/XY4hF7YDheo?si=9XTrKDChErR48YBU",
     "leandro": "https://www.youtube.com/embed/jXvUEPmE_kc?si=mVoSeMP0MxbytccO",
     "cris-e-willy": "https://www.youtube.com/embed/5iV-7ZVGn0k?si=kg1eZnWH6nZ_R5Lt",
     "cintia-e-bepo": "https://www.youtube.com/embed/aA5SaFETZ4I?si=V8tagteNsjhJ2W7K"
